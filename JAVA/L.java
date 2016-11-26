e0a12e827f9347f1dc7a97e50e19e1c99a5b6c67
Eroui <kapi.abdel@gmail.com>
2016-11-26 19:45:03 +0000

    Solution to Problem J ACM MCPC 2016

---
+++ JAVA/L.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+/**
+ * @Author Eroui Abdelaziz <kapi.abdel@gmail.com>
+ * Solution for Problem G (going) ACM MCPC 2016
+ */
+
+public class L {
+    public static void main (String[] args) {
+        Scanner in = new Scanner(System.in);
+        int T = in.nextInt();
+        
+        while(T-->0) {
+            int n = in.nextInt();
+            int count = 0;
+            for(int i = 0; i < n; i++) {
+                int tmp = in.nextInt();
+                count += tmp / 30;
+                if(tmp % 30 != 0) count+= 1;
+            }
+            
+            System.out.println(count);
+        }
+    }
+}
